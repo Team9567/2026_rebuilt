fd16ccf7659cf4b797d4733d4234437973e69334
William Page <williamtpage@gmail.com>
2026-02-03 09:05:10 -0800

    Add minimal LED subsystem using RoboWled

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,10 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.LedSubsystem;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -25,6 +28,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
+  private final LedSubsystem m_ledSubsystem = new LedSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandGenericHID m_driverController = new CommandGenericHID(OperatorConstants.kDriverControllerPort);
@@ -52,6 +56,14 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+    // Set alliance color when robot is enabled
+    new Trigger(DriverStation::isEnabled)
+        .onTrue(new InstantCommand(m_ledSubsystem::setAllianceColor, m_ledSubsystem));
+
+    // Dim LEDs when disabled
+    new Trigger(DriverStation::isDisabled)
+        .onTrue(new InstantCommand(m_ledSubsystem::setDisabled, m_ledSubsystem));
+
     m_drivetrainSubsystem.setDefaultCommand(new RunCommand(() -> {
       m_drivetrainSubsystem.arcadeDrive(-m_driverController.getRawAxis(OperatorConstants.kControllerLeftVertical),
           -m_driverController.getRawAxis(OperatorConstants.kControllerRightHorizontal));
---
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -0,0 +1,60 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.RobotBase;
+import robowled.wledpipe.DummyPipe;
+import robowled.wledpipe.SerialPipe;
+import robowled.wledpipe.WledPipe;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LedSubsystem extends SubsystemBase {
+    private WledPipe wled;
+
+    public LedSubsystem() {
+        if (RobotBase.isSimulation()) {
+            wled = new DummyPipe(msg -> System.out.println("[WLED] " + msg.trim()));
+        } else {
+            try {
+                wled = new SerialPipe(SerialPort.Port.kUSB, 115200);
+            } catch (Exception e) {
+                System.err.println("Failed to connect to WLED, using dummy");
+                wled = new DummyPipe();
+            }
+        }
+    }
+
+    public void setAllianceColor() {
+        try {
+            var alliance = DriverStation.getAlliance();
+            if (alliance.isPresent()) {
+                if (alliance.get() == DriverStation.Alliance.Red) {
+                    // Red alliance - set LEDs to red
+                    wled.sendString("{\"seg\":[{\"col\":[[255,0,0]]}]}\n");
+                } else {
+                    // Blue alliance - set LEDs to blue
+                    wled.sendString("{\"seg\":[{\"col\":[[0,0,255]]}]}\n");
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Failed to set alliance color: " + e.getMessage());
+        }
+    }
+
+    public void setEnabled() {
+        try {
+            wled.sendString("{\"on\":true,\"bri\":255}\n");
+        } catch (Exception e) {
+            System.err.println("Failed to enable LEDs: " + e.getMessage());
+        }
+    }
+
+    public void setDisabled() {
+        try {
+            wled.sendString("{\"on\":true,\"bri\":50}\n");
+        } catch (Exception e) {
+            System.err.println("Failed to dim LEDs: " + e.getMessage());
+        }
+    }
+}
+
