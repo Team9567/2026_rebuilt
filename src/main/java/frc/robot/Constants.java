f6abdf0dee2056f3caa97575815b38e896f7bd9f
Team 9567 <Team9567@gmail.com>
2026-02-19 18:49:34 -0800

    added constants, retract/extend speeds, thresholds for both sides made fuelherdersubsystem extend and added isHomed function, tells robot to not start w forward softlimit and runs periodic frequently.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,8 +58,13 @@ public final class Constants {
     public static int kRightHerderMotor = 8;
     public static final double kPositionConversionFactor = 0;
     public static final double kMotorRampTime = 0.1;
-    public static final double kForwardSoftLimit = 5.2;
+    public static final double kLeftForwardSoftLimit = 5.2; //needs different number
     public static final double kReverseSoftLimit = 0;
+    public static final double kRightForwardSoftLimit = 5.2; //needs different number
+    public static final double kRetractArmSpeed = -0.1;
+    public static final double kExtendArmSpeed = 0.1;
+    public static final double kRightCurrentThreshold = 1;
+    public static final double kLeftCurrentThreshold = 1;
   }
 
 }
--- src/main/java/frc/robot/subsystems/FuelHerderSubsystem.java
+++ src/main/java/frc/robot/subsystems/FuelHerderSubsystem.java
@@ -2,6 +2,10 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
@@ -9,26 +13,50 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import frc.robot.Constants.FuelHerderConstants;
 
-public class FuelHerderSubsystem {
+public class FuelHerderSubsystem extends SubsystemBase {
     SparkMax leftherderMotor = new SparkMax(FuelHerderConstants.kLeftHerderMotor, MotorType.kBrushless);
     SparkMax rightherderMotor = new SparkMax(FuelHerderConstants.kRightHerderMotor, MotorType.kBrushless);
+    boolean isHomed = false;
 
     public FuelHerderSubsystem() {
         SparkMaxConfig config = new SparkMaxConfig();
         config
                 .idleMode(IdleMode.kBrake)
                 .smartCurrentLimit(20);
-        config.softLimit
-                .forwardSoftLimit(FuelHerderConstants.kForwardSoftLimit)
-                .reverseSoftLimit(FuelHerderConstants.kReverseSoftLimit)
-                .forwardSoftLimitEnabled(true)
-                .reverseSoftLimitEnabled(true);
+
         config.openLoopRampRate(FuelHerderConstants.kMotorRampTime);
         config.encoder.positionConversionFactor(FuelHerderConstants.kPositionConversionFactor);
         config.inverted(false);
+        config.softLimit
+                .forwardSoftLimit(FuelHerderConstants.kLeftForwardSoftLimit)
+                .reverseSoftLimit(FuelHerderConstants.kReverseSoftLimit)
+                .forwardSoftLimitEnabled(false)
+                .reverseSoftLimitEnabled(false);
         leftherderMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         config.inverted(true);
+        config.softLimit
+                .forwardSoftLimit(FuelHerderConstants.kRightForwardSoftLimit)
+                .reverseSoftLimit(FuelHerderConstants.kReverseSoftLimit)
+                .forwardSoftLimitEnabled(false)
+                .reverseSoftLimitEnabled(false);
         rightherderMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        setDefaultCommand(retractArms());
+    }
+
+    public Command retractArms() {
+        return run(() -> {
+            setLeft(FuelHerderConstants.kRetractArmSpeed);
+            setRight(FuelHerderConstants.kRetractArmSpeed);
+        });
+
+    }
+
+    public Command extendArms() {
+        return run(() -> {
+            setLeft(FuelHerderConstants.kExtendArmSpeed);
+            setRight(FuelHerderConstants.kExtendArmSpeed);
+        });
     }
 
     public void setLeft(double speed) {
@@ -38,5 +66,26 @@ public class FuelHerderSubsystem {
 
     public void setRight(double speed) {
         rightherderMotor.set(speed);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        if (!isHomed) {
+            // Homing
+            if (rightherderMotor.getOutputCurrent() > FuelHerderConstants.kRightCurrentThreshold
+                    && leftherderMotor.getOutputCurrent() > FuelHerderConstants.kLeftCurrentThreshold) {
+                // Motors have reached hard stop, reset encoders, enable soft limigts, and mark homing complete
+                rightherderMotor.getEncoder().setPosition(0);
+                leftherderMotor.getEncoder().setPosition(0);
+                SparkMaxConfig config = new SparkMaxConfig();
+                config.softLimit
+                        .forwardSoftLimitEnabled(true)
+                        .reverseSoftLimitEnabled(true);
+                leftherderMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+                rightherderMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+                isHomed = true;
+            }
+        }
     }
 }
